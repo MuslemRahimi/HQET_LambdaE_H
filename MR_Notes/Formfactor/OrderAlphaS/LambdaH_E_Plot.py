12ad3f2fc02ec4b3b23d6d95bd85f97395f43675
MuslemRahimi <moslem_rahimi@hotmail.de>
2020-10-15 18:50:49 +0200

    update

--- MR_Notes/Formfactor/OrderAlphaS/LambdaH_E_Plot.py
+++ MR_Notes/Formfactor/OrderAlphaS/LambdaH_E_Plot.py
@@ -46,6 +46,50 @@ lambdaH_115 = np.array(lambdaH_115)
 #=========================================================#
 #=========================================================#
 #=========================================================#
+#=========================================================#
+
+# wth for 0.85 GeV
+M_E_085 = []
+lambdaE_085 = []
+with open("LambdaE_0.85.txt", "r") as data:
+	data = data.readlines()
+	for i in data:
+		M_E_085.append(float(i.split()[0]))
+		lambdaE_085.append(float(i.split()[1]))
+
+M_E_085 = np.array(M_E_085)
+lambdaE_085 = np.array(lambdaE_085)
+#=========================================================#
+
+# wth for 1.0 GeV
+M_E_10 = []
+lambdaE_10 = []
+with open("LambdaE_1.0.txt", "r") as data:
+	data = data.readlines()
+	for i in data:
+		M_E_10.append(float(i.split()[0]))
+		lambdaE_10.append(float(i.split()[1]))
+
+M_E_10 = np.array(M_E_10)
+lambdaE_10 = np.array(lambdaE_10)
+
+#=========================================================#
+
+# wth for 1.15 GeV
+
+M_E_115 = []
+lambdaE_115 = []
+with open("LambdaE_1.15.txt", "r") as data:
+	data = data.readlines()
+	for i in data:
+		M_E_115.append(float(i.split()[0]))
+		lambdaE_115.append(float(i.split()[1]))
+
+M_E_115 = np.array(M_E_115)
+lambdaE_115 = np.array(lambdaE_115)
+#=========================================================#
+#=========================================================#
+#=========================================================#
 
 M_085 = []
 lambdaH_E_085 = []
@@ -104,6 +148,22 @@ plt.ylabel("$\lambda_{H}^{2}$ in GeV$^2$")
 plt.grid(True)
 #plt.show()
 plt.savefig("LambdaH.svg")
+#=========================================================#
+fig, ax = plt.subplots()
+
+plt.ylim(0.0,0.15)
+plt.xlim(0.20,1.0)
+ax.plot(M_E_085, lambdaE_085, "--", color='blue',label='$\omega_{th} = 0.55$ GeV')
+ax.plot(M_E_10, lambdaE_10, color='black',label='$\omega_{th} = 0.60$ GeV')
+ax.plot(M_E_115, lambdaE_115,"--",color='red',label='$\omega_{th} = 0.65$ GeV')
+
+plt.rc('legend',fontsize=12) 
+legend = ax.legend(loc='best', shadow=True)
+plt.xlabel("$M$ in GeV")
+plt.ylabel("$\lambda_{E}^{2}$ in GeV$^2$")
+plt.grid(True)
+#plt.show()
+plt.savefig("LambdaE.svg")
 #=========================================================#
 fig, ax = plt.subplots()
 
